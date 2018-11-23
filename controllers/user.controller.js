a005e643edfbf8c34407e849581bdd60944002e3
Kuldip Mishra <Kuldip.mishra@ddriven.sg>
2018-11-23 21:48:35 +0530

    fix api

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,7 @@
 
 var config = require('../config/constant');
 
-var nano = require('nano')(config.apiURL.COUCHDB_URL_SERVER);
+var nano = require('nano')(config.apiURL.COUCHDB_URL_LOCALHOST);
 var db = nano.db.use('juxtorder');
 
 
@@ -25,9 +25,9 @@ exports.addUser = async (req, res, next) => {
 exports.getUserByID = async (req, res, next) => {
     try {
   
-     var id = req.params === undefined  && req.params.id ? parseInt(req.params.id) :0;
+     var id = req.params.id ? parseInt(req.params.id) :0;
      
-        db.view('byUserid', 'new-view', {key: (id)}, function(err, body) {
+        db.view('byUserid', 'new-view', {key: id}, function(err, body) {
             if (err) console.log(err);
            
               return res.json(body)
