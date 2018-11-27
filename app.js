78b60c8f487daf593d9d15c86c4d704630dc2fdb
Kuldip Mishra <Kuldip.mishra@ddriven.sg>
2018-11-27 22:13:16 +0530

    cors enabled

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 app.all('*', function(req, res, next) {
   var origin = req.get('origin'); 
-  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Origin', 'https://juxtorder-admin.herokuapp.com');
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://juxtorder-admin.herokuapp.com");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
